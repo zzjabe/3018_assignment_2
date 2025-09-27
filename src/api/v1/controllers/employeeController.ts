08b2b6c436ae15f051dd1f51f773f7a043efcddb
Jianbin Zhang <jianbinzhang@rrc.ca>
2025-09-27 18:00:51 -0500

    Added updateEmployee function in controllers.

--- src/api/v1/controllers/employeeController.ts
+++ src/api/v1/controllers/employeeController.ts
@@ -2,19 +2,53 @@ import { Request, Response } from "express";
 import * as employeeServices from "../services/employeeServices"
 
 export const getAllEmployee = (req: Request, res: Response): void =>{
-    const employees = employeeServices.getAllEmployees;
-    res.status(200).json({
+    try {
+        const employees = employeeServices.getAllEmployees;
+        res.status(200).json({
         message: "Get all employees.",
         data: employees,
-    });
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error retriveving employees",
+        })
+    }
 };
 
 export const createEmployee = (req: Request, res: Response): void =>{
-    const newEmployee = req.body;
-    const createEmployee = employeeServices.createEmployee(newEmployee);
-    res.json({
-        message: "Employee added",
-        data: createEmployee,
-    })
+    try {
+        const newEmployee = req.body;
+        const createEmployee = employeeServices.createEmployee(newEmployee);
+        res.status(200).json({
+            message: "Employee added",
+            data: createEmployee,
+        });
+    } catch(error) {
+        res.status(500).json({
+            message: "Error cteating employee",
+        });
+    }
 };
-    
+
+export const updateEmployee = (req: Request, res: Response): void =>{
+    try {
+        const id = Number(req.params.id);
+        const updateData = req.body;
+        const updateEmployee = employeeServices.updateEmployee(id, updateData);
+        if (updateEmployee){
+            res.status(200).json({
+            message: "Employee updated",
+            data: updateEmployee,
+            });
+        } else {
+            res.status(404).json({
+                message: "Employee not found",
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "Error updating employee",
+        });
+    }
+};
+
