c6bbf70d83b52830f6df00405ed95e2a42cb3195
Jianbin Zhang <jianbinzhang@rrc.ca>
2025-09-27 16:01:12 -0500

    Added createEmployee function in controllers.

--- src/api/v1/controllers/employeeController.ts
+++ src/api/v1/controllers/employeeController.ts
@@ -1,1 +1,20 @@
 import { Request, Response } from "express";
+import * as employeeServices from "../services/employeeServices"
+
+export const getAllEmployee = (req: Request, res: Response): void =>{
+    const employees = employeeServices.getAllEmployees;
+    res.status(200).json({
+        message: "Get all employees.",
+        data: employees,
+    });
+};
+
+export const createEmployee = (req: Request, res: Response): void =>{
+    const newEmployee = req.body;
+    const createEmployee = employeeServices.createEmployee(newEmployee);
+    res.json({
+        message: "Employee added",
+        data: createEmployee,
+    })
+};
+    
