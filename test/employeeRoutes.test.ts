720b2fcc07cf00c09bb2038159e056fa91c9a23e
Jianbin Zhang <jianbinzhang@rrc.ca>
2025-09-27 23:14:53 -0500

    Done with getAllEmployee endpoint test.

--- test/employeeRoutes.test.ts
+++ test/employeeRoutes.test.ts
@@ -1,12 +1,12 @@
 import request from "supertest";
 import app from "../src/app";
 
-describe("Employee API Endpoints", () => {
+describe("creatEmployee Endpoints", () => {
     it("should call createEmployee controller", async () => {
         const mockEmployee = {
             name: "Jianbin Zhang",
             position: "Developer",
-            department: "IT",
+            department: "ADD",
             email: "jianbinzhang@rrc.ca",
             phone: "204-555-1234",
             branchId: 3
@@ -20,7 +20,7 @@ describe("Employee API Endpoints", () => {
     it("should return 400 if name is missing", async () => {
         const mockEmployee = {
             position: "Developer",
-            department: "IT",
+            department: "ADD",
             email: "jianbinzhang@rrc.ca",
             phone: "204-555-1234",
             branchId: 3
@@ -28,5 +28,28 @@ describe("Employee API Endpoints", () => {
         const res = await request(app).post("/employees").send(mockEmployee);
         expect(res.status).toBe(400);
         expect(res.body.message).toBe("Missing required fields");
+    });
+});
+
+describe("getAllEmployees endpoint", () => {
+    it("should return an array of all employees", async () => {
+        const res = await request(app).get("/employees");
+        expect(res.status).toBe(200);
+        expect(Array.isArray(res.body.data)).toBe(true);
+    });
+
+    it("should ensure each employee has required fields", async () => {
+        const res = await request(app).get("/employees");
+        expect(res.status).toBe(200);
+
+        res.body.data.forEach((employee: any) => {
+        expect(employee).toHaveProperty("id");
+        expect(employee).toHaveProperty("name");
+        expect(employee).toHaveProperty("position");
+        expect(employee).toHaveProperty("department");
+        expect(employee).toHaveProperty("email");
+        expect(employee).toHaveProperty("phone");
+        expect(employee).toHaveProperty("branchId");
+        });
     });
 });
