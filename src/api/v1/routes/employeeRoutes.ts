63301c86c21db95e56a2278eea60764208fdb482
Jianbin Zhang <jianbinzhang@rrc.ca>
2025-10-12 16:37:47 -0500

    Added updateEmployeeSchema and updated put router for validation.

--- src/api/v1/routes/employeeRoutes.ts
+++ src/api/v1/routes/employeeRoutes.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from "express";
 import * as employeeController from "../controllers/employeeController"
-import { createEmployeeSchema } from "../validators/employeeValidators"
+import { createEmployeeSchema, updateEmployeeSchema } from "../validators/employeeValidators"
 import { validate } from "../middlewares/validateMiddleware"
 
 const router = Router();
@@ -12,7 +12,8 @@ router.get("/:id", employeeController.getEmployee);
 
 router.post("/", validate(createEmployeeSchema), employeeController.createEmployee);
 
-router.put("/:id", employeeController.updateEmployee);
+router.put("/:id", validate(updateEmployeeSchema), employeeController.updateEmployee);
+
 router.delete("/:id", employeeController.deleteEmployee)
 
 export default router;
--- src/api/v1/validators/employeeValidators.ts
+++ src/api/v1/validators/employeeValidators.ts
@@ -4,8 +4,17 @@ import { Employee } from "../models/employeeModel";
 export const createEmployeeSchema = Joi.object<Omit<Employee, "id">>({
     name: Joi.string().min(3).max(30).required(),
     position: Joi.string().min(3).max(30).required(),
-    department: Joi.string().min(3).max(30).required(),
+    department: Joi.string().min(3).max(30).optional(),
     email: Joi.string().email().required(),
-    phone: Joi.string().min(3).max(30).required(),
+    phone: Joi.string().min(3).max(30).optional(),
     branchId: Joi.number().required()
+});
+
+export const updateEmployeeSchema = Joi.object<Omit<Employee, "id">>({
+    name: Joi.string().min(3).max(30).optional(),
+    position: Joi.string().min(3).max(30).optional(),
+    department: Joi.string().min(3).max(30).optional(),
+    email: Joi.string().email().optional(),
+    phone: Joi.string().min(3).max(30).optional(),
+    branchId: Joi.number().optional()
 });
