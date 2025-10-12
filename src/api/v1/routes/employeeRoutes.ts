b7dc059284d89c09fe2eec40b6e9fd17fe1332f8
Jianbin Zhang <jianbinzhang@rrc.ca>
2025-10-12 16:33:57 -0500

    Updated post router for validation.

--- src/api/v1/routes/employeeRoutes.ts
+++ src/api/v1/routes/employeeRoutes.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from "express";
 import * as employeeController from "../controllers/employeeController"
+import { createEmployeeSchema } from "../validators/employeeValidators"
+import { validate } from "../middlewares/validateMiddleware"
 
 const router = Router();
 
@@ -7,7 +9,9 @@ router.get("/", employeeController.getAllEmployees);
 router.get("/byBranch", employeeController.getByBranch);
 router.get("/byDepartment", employeeController.getByDepartment);
 router.get("/:id", employeeController.getEmployee);
-router.post("/", employeeController.createEmployee);
+
+router.post("/", validate(createEmployeeSchema), employeeController.createEmployee);
+
 router.put("/:id", employeeController.updateEmployee);
 router.delete("/:id", employeeController.deleteEmployee)
 
