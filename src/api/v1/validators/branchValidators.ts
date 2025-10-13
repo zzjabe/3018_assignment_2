b49043ac1380276cc2eb1651f821a6ed60784d69
Jianbin Zhang <jianbinzhang@rrc.ca>
2025-10-12 21:45:40 -0500

    Updated employeeValidators.

--- src/api/v1/validators/branchValidators.ts
+++ src/api/v1/validators/branchValidators.ts
@@ -2,13 +2,13 @@ import Joi from "joi";
 import { Branch } from "../models/branchModel";
 
 export const createBranchSchema = Joi.object<Omit<Branch, "id">>({
-    name: Joi.string().min(3).max(30).required(),
-    address: Joi.string().min(3).max(30).required(),
-    phone: Joi.string().min(3).max(30).required()
+    name: Joi.string().min(1).max(30).required(),
+    address: Joi.string().min(1).max(300).required(),
+    phone: Joi.string().min(1).max(30).required()
 });
 
 export const updateBranchSchema = Joi.object<Omit<Branch, "id">>({
-    name: Joi.string().min(3).max(30).optional(),
-    address: Joi.string().min(3).max(30).optional(),
-    phone: Joi.string().min(3).max(30).optional()
+    name: Joi.string().min(1).max(30).optional(),
+    address: Joi.string().min(1).max(300).optional(),
+    phone: Joi.string().min(1).max(30).optional()
 });
--- src/api/v1/validators/employeeValidators.ts
+++ src/api/v1/validators/employeeValidators.ts
@@ -3,8 +3,8 @@ import { Employee } from "../models/employeeModel";
 
 export const createEmployeeSchema = Joi.object<Omit<Employee, "id">>({
     name: Joi.string().min(3).max(30).required(),
-    position: Joi.string().min(3).max(30).required(),
-    department: Joi.string().min(3).max(30).optional(),
+    position: Joi.string().min(3).max(300).required(),
+    department: Joi.string().min(1).max(30).optional(),
     email: Joi.string().email().required(),
     phone: Joi.string().min(3).max(30).optional(),
     branchId: Joi.number().required()
@@ -12,8 +12,8 @@ export const createEmployeeSchema = Joi.object<Omit<Employee, "id">>({
 
 export const updateEmployeeSchema = Joi.object<Omit<Employee, "id">>({
     name: Joi.string().min(3).max(30).optional(),
-    position: Joi.string().min(3).max(30).optional(),
-    department: Joi.string().min(3).max(30).optional(),
+    position: Joi.string().min(3).max(300).optional(),
+    department: Joi.string().min(1).max(30).optional(),
     email: Joi.string().email().optional(),
     phone: Joi.string().min(3).max(30).optional(),
     branchId: Joi.number().optional()
